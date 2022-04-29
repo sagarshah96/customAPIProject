c39b7e6a6d1f62bcb139e14a06e4a7a5bdf4c092
sagarshah96 <sagarkshah96@gmail.comm>
2022-04-29 12:13:36 +0530

    Genric repo Sample

---
+++ CustomAPIProject/Repository/GenericRepository/AllRepository.cs
@@ -0,0 +1,53 @@
+﻿using CustomAPIProject.ApplicationContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomAPIProject.Repository.GenericRepository
+{
+    public class AllRepository<T> : IAllRepository<T> where T : class
+    {
+
+        private readonly DBContext _dataContext;
+
+        private readonly DbSet<T> DbEntity;
+        public AllRepository(DBContext dbContext)
+        {
+            _dataContext = dbContext;
+            DbEntity = _dataContext.Set<T>();
+        }
+
+        public void DeleteModel(T ModelId)
+        {
+            T model = DbEntity.Find(ModelId);
+            DbEntity.Remove(model);
+        }
+
+        public IEnumerable<T> GetModel()
+        {
+            return DbEntity.ToList();
+        }
+
+        public T GetModelById(int ModelId)
+        {
+            return DbEntity.Find(ModelId);
+        }
+
+        public void InsertModel(T model)
+        {
+            DbEntity.Add(model);
+        }
+
+        public void Save()
+        {
+            _dataContext.SaveChanges();
+        }
+
+        public void UpdateModel(T model)
+        {
+            _dataContext.Entry(model).State = EntityState.Modified;
+        }
+    }
+}
---
+++ CustomAPIProject/Repository/GenericRepository/IAllRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomAPIProject.Repository.GenericRepository
+{
+    interface IAllRepository<T> where T : class
+    {
+        IEnumerable<T> GetModel();
+        T GetModelById(int ModelId);
+        void InsertModel(T model);
+        void UpdateModel(T model);
+        void DeleteModel(T ModelId);
+        void Save();
+    }
+}
